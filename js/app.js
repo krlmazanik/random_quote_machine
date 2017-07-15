4dad0a9530d6976553bd54e5ce14aa4cc4e6cb21
krlmazanik <krlmazanik@gmail.com>
2017-07-15 11:46:59 +0200

    added animation

--- js/app.js
+++ js/app.js
@@ -1,16 +1,21 @@
 var main = function(){
-  $('#getMessage').on('click', function(){
-    $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
-        .done(update);
-    });
+  $('#getMessage').on('click', getQuote);
+
+    function getQuote(){
+      $.getJSON("https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?")
+          .done(update);
+    }
 
     function update(response) {
+      $('#quote').hide().fadeIn(600).show();
+      $('#author').hide().fadeIn(600).show();
       $('#quote').html(JSON.stringify(response.quoteText));
       if(response.quoteAuthor == ""){
         response.quoteAuthor = "Unknown author";
       }
       $('#author').html(JSON.stringify(response.quoteAuthor));
     }
+    $(document).ready(getQuote);
 };
 
 $(document).ready(main);
